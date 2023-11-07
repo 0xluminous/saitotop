3e57e0c22e02ac4ce0793c686ae4dbfb83839a5b
0xluminous <0xluminous@protonmail.com>
2023-11-06 21:29:29 -0800

    deciding ui

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const log = require("debug")("saitotop:index");
 const Stats = require("./stats").default;
+const UI = require("./ui").default;
 
 async function main() {
-    const stats = new Stats(process.argv[2]);
-    stats.start();
+    const ui = new UI();
+    // const stats = new Stats(process.argv[2]);
+    // stats.start();
+
     /*
     const stats_file = process.argv[2];
 
---
+++ src/ui.js
@@ -0,0 +1,9 @@
+const log = require("debug")("saitotop:ui");
+const gui = require("console-gui-tools");
+
+
+export default class UI {
+    constructor() {
+        log("Initializing UI");
+    }
+}
