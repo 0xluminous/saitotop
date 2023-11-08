fad7363c9a28068177fb4229bb9a5ed1fa768e62
0xluminous <0xluminous@protonmail.com>
2023-11-07 17:54:51 -0800

    got network working again

--- src/components/network.js
+++ src/components/network.js
@@ -9,6 +9,7 @@ export default class NetworkComponent {
             height: "20%",
             width: "80%",
             padding: { top: 0, left: 1, right: 1, bottom: 0 },
+
             tags: true,
             content: "Initializing...",
         });
@@ -39,35 +40,21 @@ export default class NetworkComponent {
             const stat = event["stats"]["current target"];
             const value = truncateHash(stat.value, 4);
             const last_value = stat.last_value ? `${truncateHash(stat.last_value, 4)} ` : "";
-            // stat.color = "blue";
             const color = stringToColor(stat.value);
             content += `{white-fg}{bold}TARGET:  {${color}-bg}{black-fg}  {/black-fg}{/${color}-bg} ${value}{/white-fg}{/bold} {gray-fg}${last_value}${timeago(stat.date)} ago{/gray-fg}\n`;
         }
 
-        /*
-        if (this.data["mining::golden_tickets"]) {
-            content += "\n";
-            const target = data["mining::golden_tickets"]["params"]["current target"];
-            const color1 = `#${target.substring(0, 6)}`;
-            const color2 = invert(color1);
-            content += `{bold}TARGET:  {${color1}-fg}{${color2}-bg}${data["mining::golden_tickets"]["params"]["current target"]}{/bold}{/${color2}-bg}{/${color1}-fg}`;
+        if (event = stats["mining::golden_tickets"]) {
+            const stat = event["stats"]["miner_active"];
+            const value = (stat.value ? "{white-fg}{green-bg}MINING{/green-bg}{/white-fg}" : "{white-fg}{red-bg}NOT MINING{/red-bg}{/white-fg}");
+            content += `{white-fg}{bold}MINING:  ${value}{/bold}{/white-fg}\n`;
         }
 
-        if (data["mining::golden_tickets"]) {
-            content += "\n";
-
-            if (data["mining::golden_tickets"]["params"]["miner_active"] === true) {
-                content += '{bold}MINING:  {white-fg}{green-bg}YES{/bold}{/green-bg}{/white-fg}';
-            } else {
-                content += '{bold}MINING:  {white-fg}{red-bg}NO{/bold}{/red-bg}{/white-fg}';
-            }
+        if (event = stats["mempool::state"]) {
+            const transactions = event["stats"]["transactions"]["value"];
+            const blocks = event["stats"]["blocks_queue"]["value"];
+            content += `{white-fg}{bold}MEMPOOL: tx=${transactions} blk=${blocks}{/bold}{/white-fg}`;
         }
-
-        if (data["mempool::state"]) {
-            content += "\n";
-            content += `{bold}MEMPOOL: tx=${data["mempool:state"]["params"]["transactions"]} blk=${data["mempool:state"]["params"]["blocks_queue"]}{/bold}`;
-        }
-        */
 
         this.component.setContent(content);
     }
